0e5238fb6d87ab72dd47572434ab790231063181
EvripB <ebourlas@gmail.com>
2026-04-24 20:00:10 +0300

    create cache dir if it doesn't exist

--- Voron/Talking Voron/ttsserver.py
+++ Voron/Talking Voron/ttsserver.py
@@ -15,6 +15,7 @@ def speak_text(text):
     key = safe_text.lower().strip()
     key = " ".join(key.split())
     key = re.sub(r'[^a-z0-9 _-]', '', key)
+    os.makedirs("/home/pi/talking_voron/cache", exist_ok=True)
     filename = "/home/pi/talking_voron/cache/" + key.replace(" ", "_") + ".wav"
 
     if not os.path.exists(filename):
